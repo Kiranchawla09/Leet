41ec432cce15826799fae625cf54cfed494c664b
Kiran Chawla <44634804+Kiranchawla09@users.noreply.github.com>
2020-03-21 15:25:17 +0530

    Array

---
+++ Pascals_Triangle_II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+       vector <int> vect(rowIndex+1);
+        vect[0]=1;
+        for (int i=0;i<=rowIndex;i++)
+        {
+            for (int j=i;j>0;j--)
+            {
+                vect[j]= vect[j-1]+vect[j];
+            }
+        }
+        return vect;
+    }
+};
