fed01e508519d5bf7461d733c732b213e2f430a8
Kiran Chawla <44634804+Kiranchawla09@users.noreply.github.com>
2020-05-11 12:03:46 +0530

    Hash Map

---
+++ Find_the_difference.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        vector<int> f(26, 0);
+        for (char c : t)
+            f[c-'a']++;
+        for (char c : s)
+            f[c - 'a']--;
+        for (int i = 0; i < 26; i++) {
+            if (f[i])
+                return i + 'a';
+        }
+        return 0;
+    }
+};
