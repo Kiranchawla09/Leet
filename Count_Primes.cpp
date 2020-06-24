c6d294b01181cdc5e83f775945a673984e36364e
Kiran Chawla <44634804+Kiranchawla09@users.noreply.github.com>
2020-06-24 12:05:30 +0530

    Miscellaneous

---
+++ Count_Primes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        int count=0;    
+        for (int i=0;i<n;i++)
+        {
+            if (isPrime(i))
+                count++;
+        }
+        return count;
+    }
+    bool isPrime (int n)
+    {
+        if (n<=1)
+            return false;
+        if (n<=3)
+            return true;
+        if (n%2==0 || n%3==0)
+            return false;
+        for (int i=5;i*i<=n;i=i+6)
+        {
+            if (n%i==0 || n%(i+2)==0)
+                return false;
+        }
+        return true;
+    }
+};
